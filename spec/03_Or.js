f645c472675004fb0c6563b5995b7ca4b6cb9cf3
tomii <g244103@outlook.com>
2016-11-04 01:24:06 +0530

    04

--- spec/03_Or.js
+++ spec/03_Or.js
@@ -2,13 +2,13 @@ describe("Or", function() {
 
   it('pipe character ( | ) is a regex OR', function() {
     var thisPatternWorks = /^dog|cat$/;
-    var fixThisPattern   = /^___$/;
-    
+    var fixThisPattern   = /^hearts|clubs$/;
+
     expect( 'dog' ).toMatch(thisPatternWorks);
     expect( 'cat' ).toMatch(thisPatternWorks);
-    
+
     expect( 'hearts' ).toMatch(fixThisPattern);
     expect( 'clubs'  ).toMatch(fixThisPattern);
   });
-  
+
 });
