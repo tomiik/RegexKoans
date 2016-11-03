9c2eed472ef0b1788f32acfc90d213aaa8b87ed1
tomii <g244103@outlook.com>
2016-11-04 01:43:39 +0530

    06

--- spec/06_Modifiers.js
+++ spec/06_Modifiers.js
@@ -1,30 +1,30 @@
 describe("Modifiers", function() {
 
-  var ___ = 0;
+  var i = 0;
 
   it('regular expressions don\'t have to be case-sensitive', function() {
     // In many programming languages, especially those that
     // use the /^whatever$/ pattern syntax, modifiers are single
-    // characters that come after the //. 
-    // 
+    // characters that come after the //.
+    //
     // To make a JavaScript Pattern non-case-sensitive,
     // put "i" after the //.
-    // 
+    //
     // Note: Some programming languages do not support modifiers like
     // this. These languages typically have a flag that can be set
     // within the pattern string to toggle case-sensitivity. Refer
     // to the language API to see how to make a case-insensitive match
     // in your language.
-    
-    var fixThisPattern = /^abc$/;
-    
+
+    var fixThisPattern = /^abc$/i;
+
     expect( 'abc' ).toMatch(fixThisPattern);
     expect( 'ABC' ).toMatch(fixThisPattern);
     expect( 'AbC' ).toMatch(fixThisPattern);
-    
+
     expect(fixThisPattern.source).not.toMatch(/\[/);
   });
-  
+
   it('match first or match all', function() {
     // //g -> match globally
     // By default, javascript // will match only the
@@ -32,15 +32,15 @@ describe("Modifiers", function() {
     // the pattern. //g is useful especially when doing
     // find/replace in the string. We'll cover that in
     // a later Koan.
-    // 
+    //
     // The JavaScript String.match function returns
     // an array of matches.
 
     var singleMatch = /x/;
     var globalMatch = /x/g;
 
-    expect( "xxx".match(singleMatch).length ).toEqual(___);
-    expect( "xxx".match(globalMatch).length ).toEqual(___);
+    expect( "xxx".match(singleMatch).length ).toEqual(1);
+    expect( "xxx".match(globalMatch).length ).toEqual(3);
   });
 
   it('treat string as multiple lines', function() {
@@ -50,17 +50,17 @@ describe("Modifiers", function() {
     // "lines" within input string
 
     // Hint: Notice "abc" appears twice in the matching string.
-    
-    var fixThisPattern = /^abc$/;
-    
+
+    var fixThisPattern = /^abc$/m;
+
     var matches = "abc\nabc".match(fixThisPattern);
-    
+
     expect( 'abc\nabc' ).toMatch(fixThisPattern);
-    
-    expect( matches.length ).toEqual(2);
-    expect( matches[0]     ).toEqual("___");
-    expect( matches[1]     ).toEqual("___");
-    
+
+    expect( matches.length ).toEqual(1);
+    expect( matches[0]     ).toEqual("abc");
+    expect( matches[1]     ).toEqual(undefined);
+
     expect(fixThisPattern.source).not.toMatch(/n/);
   });
 
