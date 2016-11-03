302eced28b5147a6cec185c59299bea1a17a6cd3
tomii <g244103@outlook.com>
2016-11-04 02:17:16 +0530

    08

--- spec/09_Grouping.js
+++ spec/09_Grouping.js
@@ -4,45 +4,45 @@ describe("Grouping", function() {
   // with single characters. All regular expression operators can also be
   // applied to sequences by grouping the sequence inside ( ) like
   // "ababababab" =~ /^(ab)+$/
-  
+
   it('the cat is optional', function() {
-    
-    var fixThisPattern = /^DogCatPony$/;
-    
+
+    var fixThisPattern = /^Dog(Cat)?Pony$/;
+
     expect( 'DogCatPony' ).toMatch(fixThisPattern);
     expect( 'DogPony'    ).toMatch(fixThisPattern);
   });
-  
+
   it('anything goes as long as we get to keep the pony', function() {
-    
-    var fixThisPattern = /^DogCatPony$/;
-    
+
+    var fixThisPattern = /^(Dog)*?(Cat)?Pony$/;
+
     expect( 'DogCatPony'          ).toMatch(fixThisPattern);
     expect( 'DogPony'             ).toMatch(fixThisPattern);
     expect( 'CatPony'             ).toMatch(fixThisPattern);
     expect( 'DogDogDogDogCatPony' ).toMatch(fixThisPattern);
   });
-  
+
   it('you can nest regular expression operations inside ( )', function() {
-    
-    var fixThisPattern = /^DogCatPony$/;
-    
+
+    var fixThisPattern = /^(Dog)*?(Cat)?s?Pony$/;
+
     expect( 'DogCatPony'           ).toMatch(fixThisPattern);
     expect( 'DogCatsPony'          ).toMatch(fixThisPattern);
     expect( 'DogPony'              ).toMatch(fixThisPattern);
     expect( 'CatPony'              ).toMatch(fixThisPattern);
     expect( 'DogDogDogDogCatsPony' ).toMatch(fixThisPattern);
   });
-  
+
   it('you can nest ( ) inside ( )', function() {
-    
-    var fixThisPattern = /^___$/;
-    
+
+    var fixThisPattern = /^(ab(cd)?)*$/;
+
     expect( 'abcd'           ).toMatch(fixThisPattern);
     expect( 'abcdabcdabcd'   ).toMatch(fixThisPattern);
     expect( 'ababababcdabab' ).toMatch(fixThisPattern);
   });
-  
+
   it('Real World 3: very VERY basic email address verification', function() {
     // Assumptions:
     //   * email address is composed of [username]@[domain]
@@ -51,7 +51,7 @@ describe("Grouping", function() {
     //   * domain is composed of [letters/numbers].[letters/numbers]....repeat
     //   * in domain, . is the separator between each unit. Domain cannot begin or end with .
     //   * email address will be all in lowercase
-    // 
+    //
     // Note that this is an oversimplification of the real email address
     // syntax requirements. In fact, using a regular expression to check
     // the full email address specification (http://www.ietf.org/rfc/rfc822.txt)
@@ -60,24 +60,24 @@ describe("Grouping", function() {
     // to not over-engineer. Here, we're agreeing that these are the only
     // rules we care about for email addresses, so our regex doesn't need
     // to worry about all the other stuff.
-    
-    var fixThisPattern = /^___$/;
-    
+
+    var fixThisPattern = /^[a-z.+0-9\-_]*@[a-z+0-9]*([.][a-z+0-9]+)+$/;
+
     // Hint: Develop the regex in pieces. First make a pattern that
     // matches the username part, then one section of the domain,
     // then allow multiple domain sections, then glue it all together.
-    // 
+    //
     // Just like writing a program, a complicated regular expression
     // can often be broken into smaller, simpler pieces and then
     // assembled into the final whole.
-    
+
     // valid
     expect( 'myname@internet.com'                      ).toMatch(fixThisPattern);
     expect( 'my_name2@server16.subdomain.internet.org' ).toMatch(fixThisPattern);
     expect( 'jdoe87@college.edu'                       ).toMatch(fixThisPattern);
     expect( 'jdoe87+myalias@gmail.com'                 ).toMatch(fixThisPattern);
     expect( 'my.really.long.first-name@internet.co.uk' ).toMatch(fixThisPattern);
-    
+
     // invalid
     expect( 'myname@internet..com'             ).not.toMatch(fixThisPattern);
     expect( 'myname@internet'                  ).not.toMatch(fixThisPattern);
